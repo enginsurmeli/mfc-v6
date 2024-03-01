dbf5809c9c2cdef8f20c2ed20e3f10f244564fa5
Engin Can Sürmeli <surmeli.engincan@gmail.com>
2024-03-01 08:17:54 +0300

    Refactor main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 #include "config.h"
 
-void setup()
+int main()
 {
   Serial.begin(115200);
   Serial.println("Initializing...");
   setupDisplay();
   setupPID();
   Serial.println("Device is ready.");
-}
-
-void loop()
-{
-  serialListen();
-  readFlowRate();
-  runPID();
-  serialStream();
-  updateDisplay();
-  delay(5);
+  while (1)
+  {
+    serialListen();
+    readFlowRate();
+    runPID();
+    serialStream();
+    updateDisplay();
+    delay(5);
+  }
+  return 0;
 }
